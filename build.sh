8a327838d6cdbee7fa6cbe1c01c4a6f3de6e99ee
Darren Gallagher <d.gallagher@qureight.com>
2021-09-23 18:01:34 +0100

    Upgrade to 4.11.20210226

--- build.sh
+++ build.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+set -e
 
-VERSION="4.11.20200930"
 
-docker build --build-arg VERSION=${VERSION} -t stevepieper/base:$VERSION base
-docker build --build-arg VERSION=${VERSION} -t stevepieper/x11:$VERSION x11
-docker build --build-arg VERSION=${VERSION} -t stevepieper/slicer:$VERSION slicer
+VERSION="4.11.20210226"
+
+#docker build --build-arg VERSION=${VERSION} -t dazzag24/base:$VERSION base
+#docker build --build-arg VERSION=${VERSION} -t dazzag24/x11:$VERSION x11
+docker build --build-arg VERSION=${VERSION} -t dazzag24/slicer:$VERSION slicer
+exit 1
 
 SLICER_MORPH_EXTS="MarkupsToModel Auto3dgm SegmentEditorExtraEffects Sandbox SlicerIGT RawImageGuess SlicerDcm2nii SurfaceWrapSolidify SlicerMorph"
 
 docker build \
   --build-arg VERSION=${VERSION} --build-arg SLICER_EXTS="${SLICER_MORPH_EXTS}" \
-  -t stevepieper/slicer-morph:$VERSION slicer-plus
+  -t dazzag24/slicer-morph:$VERSION slicer-plus
 
 SLICER_DMRI_EXTS="ukftractography SlicerDMRI"
 
 docker build \
   --build-arg VERSION=${VERSION} --build-arg SLICER_EXTS="${SLICER_DMRI_EXTS}" \
-  -t stevepieper/slicer-dmri:$VERSION slicer-plus
+  -t dazzag24/slicer-dmri:$VERSION slicer-plus
 
-docker build --build-arg VERSION=${VERSION} -t stevepieper/slicer-dev:$VERSION slicer-dev
+docker build --build-arg VERSION=${VERSION} -t dazzag24/slicer-dev:$VERSION slicer-dev
 
 
-docker build -t stevepieper/slicer3:$VERSION slicer3
-docker build --build-arg VERSION=${VERSION} --no-cache -t stevepieper/slicer-chronicle:$VERSION slicer-chronicle
+docker build -t dazzag24/slicer3:$VERSION slicer3
+docker build --build-arg VERSION=${VERSION} --no-cache -t dazzag24/slicer-chronicle:$VERSION slicer-chronicle
 
